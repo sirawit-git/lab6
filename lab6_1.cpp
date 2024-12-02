3623e8337b365e4c05be83dbf2978a7db6cbef87
Champ <champsrw49@gmail.com>
2024-12-02 22:52:56 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,25 @@
 using namespace std;
 
 int main(){
+    int countEven = 0, countOdd = 0, num;
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    cin >> num;
+    if(num==0) {
+        cout << "#Even numbers = 0" << endl;
+        cout << "#Odd numbers = 0" << endl; 
+
+    }else{
+        while(num!=0){
+         if (num % 2 == 0) {
+                countEven++;
+            } else { 
+                countOdd++;
+            }
+                cout << "Enter an integer: ";
+                cin >> num;
+    }
+    cout << "#Even numbers = "<< countEven << endl;
+    cout << "#Odd numbers = "<< countOdd << endl;
+    }
     return 0;
 }
--- lab6_2.cpp
+++ lab6_2.cpp
@@ -1,9 +1,45 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
-int main(){
-    double l1,l2,a1,a2,xcomp,ycomp,result_vec_length,result_vec_direction;
+
+double deg2rad(double degree) {
+    return degree * (M_PI / 180.0);
+}
+
+
+double rad2deg(double radian) {
+    return radian * (180.0 / M_PI);
+}
+
+
+double findXComponent(double l1, double l2, double a1, double a2) {
+    return (l1 * cos(a1)) + (l2 * cos(a2));
+}
+
+
+double findYComponent(double l1, double l2, double a1, double a2) {
+    return (l1 * sin(a1)) + (l2 * sin(a2));
+}
+
+
+double pythagoras(double xcomp, double ycomp) {
+    return sqrt(xcomp * xcomp + ycomp * ycomp);
+}
+
+
+void showResult(double magnitude, double direction) {
+    cout << "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%" << endl;
+    cout << "Length of the resultant vector = " << magnitude << endl;
+    cout << "Direction of the resultant vector (deg) = " << direction << endl;
+    cout << "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%" << endl;
+}
+
+int main() {
+    double l1, l2, a1, a2, xcomp, ycomp, result_vec_length, result_vec_direction;
+
+
     cout << "Enter length of the first vector: ";
     cin >> l1;
     cout << "Enter direction of the first vector (deg): ";
@@ -12,13 +48,23 @@ int main(){
     cin >> l2;
     cout << "Enter direction of the second vector (deg): ";
     cin >> a2;
-    
+
+
     a1 = deg2rad(a1);
     a2 = deg2rad(a2);
-    xcomp = findXComponent(l1,l2,a1,a2);
-    ycomp = findYComponent(l1,l2,a1,a2);
-    result_vec_length = pythagoras(xcomp,ycomp);
-    result_vec_direction = rad2deg(atan2(ycomp,xcomp)); 
 
-    showResult(result_vec_length,result_vec_direction);
+
+    xcomp = findXComponent(l1, l2, a1, a2);
+    ycomp = findYComponent(l1, l2, a1, a2);
+
+
+    result_vec_length = pythagoras(xcomp, ycomp);
+
+
+    result_vec_direction = rad2deg(atan2(ycomp, xcomp));
+
+
+    showResult(result_vec_length, result_vec_direction);
+
+    return 0;
 }
